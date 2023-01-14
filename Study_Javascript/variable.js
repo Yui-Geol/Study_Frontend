4a51afbcd03dcfdfe2540b1a25995a8326936976
Yui-Geol <gamoon777@naver.com>
2023-01-15 02:26:22 +0900

    add variable.js

---
+++ Study_Javascript/variable.js
@@ -0,0 +1,57 @@
+//variable is a name given to identify the memory space itself or its memory space that is free to store a single value
+//varibale is a sybolical name that point out memory location
+//variable name is identifier. idnetifier is a unique name that distinguish some value to identify
+//identifier remembers not value but memory address
+//declaration is needed when we use variable. when we declare the variable, we use let, const and var.
+//undefined is primitive value that provide at javascript
+
+//10 + 20 is expression
+//10 and 20 are operand
+//+ is operator
+
+console.log(test);//undefined
+
+var test = "hello";//test is hoisted before runtime
+
+console.log(test);//hello
+
+console.log(score);
+
+score = 80;
+var score; 
+
+console.log(score);
+
+//constant should be assigned the value when declare the constant
+const DIGIT = 10;
+
+//naming convention
+
+//camel case
+var firstName;
+
+//snake case
+var firstName;
+
+//pascal case
+var FirstName;
+
+//variable and function name ==> camel case
+//class and constructor function name ==> pascal case
+
+100//integer literal(decimal)
+10.5//Floating-point literal
+0b0100001//binary literal
+0o10011//octal literal
+0x11011//hexadecimal literal
+'hello'//string literal
+true//boolean literal
+null//null literal
+undefined//undefined literal
+const a = { name: 'choi', adress: 'ulsan' }//object literal
+[ 1, 2, 3 ]//array literal
+//function(){} function literal
+//[A-Z]+/g regular expression literal
+
+//All statement that can be evaluated by value are expressions
+
