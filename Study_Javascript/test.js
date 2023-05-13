37ca0364055bef4bcc2f6b02cbba064af23e8ad6
CHOI YUI GEOL <gamoon777@naver.com>
2023-05-14 01:10:35 +0900

    fix

--- Study_Javascript/test.js
+++
@@ -1,9 +0,0 @@
-function multiply(x, y) {
-	console.log(arguments);
-	return x + y;
-}
-
-console.log(multiply());
-console.log(multiply(1));
-console.log(multiply(1,2));
-console.log(multiply(1,2,3));
